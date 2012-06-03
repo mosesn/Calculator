a355e14190f9871b2a2f5df7f64366d7031dbd0e
Moses Nakamura <nnythm@gmail.com>
2012-06-03 14:01:22 -0400

    Uses a tree and everything now, but operator precedence and associativity is fucked

--- src/main/scala/Calculator.scala
+++ src/main/scala/Calculator.scala
@@ -4,26 +4,51 @@ object Calculator extends RegexParsers {
 
   val NUMBER = """\d+""".r
 
-  def int: Parser[Int] = NUMBER ^^ { _.toInt }
-
-  def plus: Parser[Int] = "+" ~ expr ^^ {
-    case "+" ~ elt => elt
-  }
-  
-  def minus: Parser[Int] = "-" ~ expr ^^ {
-    case "-" ~ elt => -elt
-  }
-  
-  def changer: Parser[Int] = plus | minus
-  
-  def expr: Parser[Int] = int ~ rep(changer) ^^ {
-    case first ~ second => first + second.sum
+  def int: Parser[NumberNode] = NUMBER ^^ {
+    case elt => NumberNode(elt.toInt)
   }
 
-  def eval: Parser[Int] = expr
+  def parens: Parser[Node] = "(" ~ expr ~ ")" ^^ {
+    _._1._2
+  }
+  
+  def multiplication: Parser[Node] = term ~ "*" ~ expr ^^ {
+    x => OpNode("*", x._1._1, x._2)
+  }
+  
+  def addsub: Parser[Node] = add | sub
+  
+  def add: Parser[Node] = term ~ "+" ~ expr ^^ {
+    x => OpNode("+", x._1._1, x._2)
+  }
+  
+  def sub: Parser[Node] = term ~ "-" ~ expr ^^ {
+    x => OpNode("-", x._1._1, x._2)
+  }
+  
+  def term: Parser[Node] = parens | int
+
+  def expr: Parser[Node] = addsub | multiplication | term
+
+  def eval: Parser[Int] = expr ^^ {
+    _.eval
+  }
 
   def apply(input: String): Number = parseAll(eval, input) match {
     case Success(result, _) => result
     case failure: NoSuccess => scala.sys.error(failure.msg)
   }
 }
+
+trait Node {
+  def eval: Int = this match {
+    case NumberNode(contents) => contents
+    case OpNode("*", left, right) => left.eval * right.eval
+    case OpNode("+", left, right) => left.eval + right.eval
+    case OpNode("-", left, right) => left.eval - right.eval
+  }
+}
+
+case class NumberNode(contents: Int) extends Node
+
+case class OpNode(contents: String, left: Node, right: Node) extends Node 
--- src/test/scala/CalculatorSpec.scala
+++ src/test/scala/CalculatorSpec.scala
@@ -7,16 +7,45 @@ class CalculatorSpec extends FunSpec {
   describe("Calculator") {
     describe("apply") {
       it("Should identify numbers") {
-        assert(Calculator("0") == 0)
+        assert(Calculator("0") === 0)
       }
       
       it("Should do addition") {
-        assert(Calculator("1 + 1") == 2)
+        assert(Calculator("1 + 1") === 2)
       }
       
       it("Should do subtraction") {
-        assert(Calculator("2 - 1") == 1)
+        assert(Calculator("2 - 1") === 1)
       }
+      
+      it("Should do iterated addition") {
+        assert(Calculator("1 + 2 + 3 + 5 + 7 + 2") === 1 + 2 + 3 + 5 + 7 + 2)
+      }
+      
+      it("Should do iterated subtraction") {
+        assert(Calculator("1 - 2 - 3 - 5 - 7 - 2") === 1 - 2 - 3 - 5 - 7 - 2)
+      }
+      
+      it("Should do interleaved addition and subtraction") {
+        assert(Calculator("1 + 2 - 3 + 5 - 7 - 2") === 1 + 2 - 3 + 5 - 7 - 2)
+      }
+
+      it("Should use parenthesization properly") {
+        assert(Calculator("1 + 2 - 3 + 5 - (7 - 2)") === 1 + 2 - 3 + 5 - (7 - 2))
+      }
+
+      it("Should handle multiplication") {
+        assert(Calculator("2 * 7") === 2 * 7)
+      }
+      
+      it("Should handle iterated multiplication") {
+        assert(Calculator("2 * 7 * 3 * 8 * 9 * 6") === 2 * 7 * 3 * 8 * 9 * 6)
+      }
+
+      it("Should handle mixed precedence levels") {
+        assert(Calculator("2 * 4 + 2 * 8 * 9 * 6") === 2 * 4 + 2 * 8 * 9 * 6)
+      }
+
     }
   }
 }
